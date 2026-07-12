e8083098ec4f20a54cfa533ab9fd049bff2954b8
Seetha-17 <seethathummepalli17@gmail.com>
2026-07-12 20:07:39 +0530

    Add constructor to Employee class

--- models/employee.py
+++ models/employee.py
@@ -1,10 +1,5 @@
 class Employee:
-    def _init_(self, name: str, email: str):
-        self.name = name.strip()
-        self.email = email.strip().lower()
-
-    def _eq_(self, other):
-        return isinstance(other, Employee) and self.email == other.email
-
-    def _hash_(self):
-        return hash(self.email)
+    def _init_(self, employee_id: str, name: str, email: str):
+        self.employee_id = employee_id
+        self.name = name
+        self.email = email
