72941e0aea9a4b5eda6d28b5495e66cb95239540
Seetha-17 <seethathummepalli17@gmail.com>
2026-07-12 23:48:15 +0530

    Fix:add working explicit constructor to Employee class

--- models/employee.py
+++ models/employee.py
@@ -1,13 +1,13 @@
 class Employee:
-    def _init_(self, name: str, email: str):
+    def __init__(self, name: str, email: str):
         self.name = name
         self.email = email
 
     # This helps Python compare employees correctly when eliminating last year's children
-    def _eq_(self, other):
+    def __eq__(self, other):
         if not isinstance(other, Employee):
             return False
         return self.email == other.email
 
-    def _hash_(self):
+    def __hash__(self):
         return hash(self.email)
